46fa0e93fa0e97007f9baf248a620a78950a6c1b
jrosalesmeza <51757560+jrosalesmeza@users.noreply.github.com>
2021-08-06 22:57:50 -0500

    Agregamos la carpeta app

---
+++ app/core/erp/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+from datetime import datetime
+# Create your models here.
+
+
+
+class Type(models.Model):
+    name= models.CharField(max_length=150, verbose_name='Nombre', unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name='Tipo'
+        verbose_name_plural='Tipos'
+        ordering=['id']
+
+class Category(models.Model):
+    name= models.CharField(max_length=150, verbose_name='Nombre')
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name='Tipo'
+        verbose_name_plural='Tipos'
+        ordering=['id']
+
+
+
+
+
+
+class Employee(models.Model):
+    Category= models.ManyToManyField(Category) 
+    type= models.ForeignKey(Type, on_delete=models.CASCADE)  #models.CASCADE=> Si llegase a eliminar la tabla, se elimina la información || ForeignKey => Tablas relacionales
+    names = models.CharField(max_length=150, verbose_name='Nombres')     #names= models.TextField() //Permite cualquier numero de caracteres
+    cedula = models.CharField(max_length=10, verbose_name='Cedula', unique=True)    #verbose_name // Sirve para nombrar la columna con algo en especifico, si no se llama como su propiedad
+    date_joined = models.DateField(default=datetime.now, verbose_name="Fecha de registro")  #default= Un valor por defecto
+    date_creation= models.DateTimeField(auto_now=True)
+    date_updated= models.DateTimeField(auto_now_add=True)  #auto now add => Se incrementa automaticamente la fecha
+    age= models.PositiveBigIntegerField(default=0, verbose_name='Edad')
+    salary=models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
+    state= models.BooleanField(default=True)
+    avatar= models.ImageField(upload_to='avatar/%Y/%m/%d',null=True, blank=True) 
+    cvitae= models.FileField(upload_to='cvitae/%Y/%m/%d',null=True, blank=True)
+    gender = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.names
+
+    class Meta:
+        verbose_name='Empleado'
+        verbose_name_plural='Empleados'
+        db_table='empleado'
+        ordering=['id']
+
---
+++ app/core/erp/tests.py
@@ -0,0 +1,41 @@
+
+from os import name
+from config.wsgi import *
+from core.erp.models import Type
+
+
+#ORM de Django
+# #Listar
+
+# #Select *from Tabla
+# query=Type.objects.all()
+
+# print(query)
+
+# #Insercion
+# t= Type(name="Prueba2").save()
+
+
+#Edición
+
+# t= Type.objects.get(id=5)
+
+# t.name= "Prueba2 Editado"
+# t.save()
+
+
+#Eliminación
+
+
+# try:
+#     t= Type.objects.get(pk=3)
+#     t.delete()
+# except Exception as e:
+#     print('No se puede elimintar el id=3 ', e)
+
+
+
+
+for i in Type.objects.filter(name__endswith='e')[:2]:
+    print(i.id)
+
